3eb66030aaf1565fbfc555c95a5f2fe99764fe41
Pri-21 <priya_now@hotmail.co.uk>
2022-03-08 11:29:08 +0000

    view an individual article

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import * as api from "../Api";
+
 export const Article = () => {
-    return ( <div>
-        
+  const { article_id } = useParams();
+  const [articleInfo, setArticleInfo] = useState({});
+  useEffect(() => {
+    api.fetchArticleById(article_id).then((articleData) => {
+      setArticleInfo(articleData);
+    });
+  }, []);
+  return (
+    <div className="article">
+      <h3>{articleInfo.title}</h3>
+      <p>{articleInfo.body}</p>
+      <p>By: {articleInfo.author}</p>
+      <p>comments: {articleInfo.comment_count}</p>
+      <p>votes: {articleInfo.votes}</p>
+      <p>{articleInfo.topic}</p>
     </div>
-    )
-}
+  );
+};
